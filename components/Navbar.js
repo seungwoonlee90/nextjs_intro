6e48369c7e02df7ef43130eba6f2694365dd0878
ethanlee <seungwoonlee90@gmail.com>
2022-07-10 09:34:07 +0900

    1.4 CSS Modules

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,16 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './Navbar.module.css';
 
 function Navbar(){
     const router = useRouter();
     return (
         <nav>
             <Link href="/">
-                <a style={{color: router.pathname === "/"?"red":"blue"}}>Home</a>
+                <a className={router.pathname === "/"? styles.active:""}>Home</a>
             </Link>
             <Link href="/about">
-                <a style={{color: router.pathname === "/about"?"red":"blue"}}>About</a>
+                <a className={router.pathname === "/about"? styles.active:""}>About</a>
             </Link>
         </nav>
     )
