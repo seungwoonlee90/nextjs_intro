2ea059326e1cdd0a751f850cbb51eea5b4c516ad
ethanlee <seungwoonlee90@gmail.com>
2022-07-12 22:59:31 +0900

    2.4 Recap

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function Home(){
           {!movies && <h4>Loading...</h4>}
           {movies?.map((movie) => (
             <div className="movie" key={movie.id}>
-                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
               <h4>{movie.original_title}</h4>
             </div>
           ))}
@@ -25,6 +25,9 @@ function Home(){
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+            cursor: pointer;
         }
         .movie img {
           max-width: 100%;
